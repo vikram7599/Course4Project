8506bf4aa6bf8f8bc46236e3b2a0e26d01bf1147
vikram7599 <vikram7599@gmail.com>
2019-09-30 00:21:10 +0530

    NewChange

--- plot1.R
+++ plot1.R
@@ -8,9 +8,9 @@ if(!exists("SCC")){
 # Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 # Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
 # for each of the years 1999, 2002, 2005, and 2008.
-
+print("This is just another change I have made see the change")
 aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
-print("This is just ot see the change")
+print("This is just to see the change")
 png('plot1.png')
 barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
 dev.off()
